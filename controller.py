c38b782d874cbd1345b68847d7ed7c24e72bb922
github username <souzajohnlee@gmail.com>
2020-09-06 20:08:24 -0700

    added console logging of routes with test_request_context

--- controller.py
+++ controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, url_for
 from markupsafe import escape
 app = Flask(__name__)
 
@@ -16,8 +16,12 @@ def projects_show(project_id):
     })
 
 @app.route('/projects')
-def projects():
+def projects_index():
     return jsonify({
         "route": "/projects"
     })
 
+with app.test_request_context():
+    print(url_for('hello_world'))
+    print(url_for('projects_show', project_id=1))
+    print(url_for('projects_index'))
