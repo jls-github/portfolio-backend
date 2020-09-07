c7cceaac20ae0c3922892528624804dbca4965d1
github username <souzajohnlee@gmail.com>
2020-09-06 19:58:38 -0700

    fixed typo

--- controller.py
+++ controller.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from markupsafe import escape
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,6 +8,12 @@ def hello_world():
         "route": "/"
     })
 
+
+@app.route('/projects/<project_id>')
+def projects_show(project_id):
+    return jsonify({
+        "route": '/projects/%s' % escape(project_id)
+    })
 
 @app.route('/projects')
 def projects():
